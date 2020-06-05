1a8fd640fece8309591893134f33ae9ce72f3007
dairis <dairisdombrovskis@gmail.com>
2020-06-05 20:47:33 +0300

    added default image to new users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_picture')->default("/img/default_profile.jpg");
             $table->rememberToken();
             $table->timestamps();
         });
