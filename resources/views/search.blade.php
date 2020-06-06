2730858ad100b40cfd0795e8de62a02e1e5dc587
Bumbieris <kaspars.rumkovskis@gmail.com>
2020-06-06 15:20:06 +0300

    created search bars

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,7 +5,7 @@
 
 
 <div class="container" style="background-color: #eeeeee">
-    <button type="button" class="btn active">back</button>
+    <a href="{{{ URL::route('home') }}}" type="button" class="btn active border-dark ">Back</a>
     <form class="form-group">
         <div class="row">
             <p class="col-3 text-center">Modelis</p>
@@ -19,7 +19,7 @@
             <p class="col-3 text-center">Aviokompānija</p>
             <input class="col-9 form-control">
         </div>
-        <div class="h-200">
+        <div>
             <button class="col-12 btn-lg" type="submit">Search</button>
         </div>
         
