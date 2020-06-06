44826fd3ff4b13cb7d61a1010be86aaf4fc8e8c3
Bumbieris <kaspars.rumkovskis@gmail.com>
2020-06-06 14:14:29 +0300

    created search bars

---
+++ resources/views/search.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+
+
+
+<div class="container">
+    <button border="round">back</button>
+    <p>Modelis<input></p>
+    <p>Lidosta<input class="col-6"></p>
+    <p>Aviokompānija<input></p>
+</div>
+
+@endsection
