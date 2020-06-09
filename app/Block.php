c2e3e53f695c9102e4738cea2566bfbf7b1ca978
dairis <dairisdombrovskis@gmail.com>
2020-06-09 19:28:48 +0300

    Blocked model done

--- app/Block.php
+++ app/Block.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Block extends Model
 {
-    //
+    public function blocker() {
+        return $this->belongsTo('App/User', 'id', 'blocker_id');
+    }
+
+    public function blocked() {
+        return $this->belongsTo('App/User', 'id', 'blocked_id');
+    }
 }
