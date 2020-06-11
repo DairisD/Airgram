8767c09a087039e16ce8b120dcd47e11057dd7dc
dairis <dairisdombrovskis@gmail.com>
2020-06-12 00:03:30 +0300

    Votes controller started making

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Vote;
+use Redirect;
 
 class VotesController extends Controller
 {
-    //
+    public function inc($user, $image) {
+        $votes = DB::select("select * from votes where user_id = '$user' and image_id = '$image'");
+        if($votes) {
+            if($votes->val == 1) {
+                return Redirect::back();
+            }
+        }
+    }
 }
